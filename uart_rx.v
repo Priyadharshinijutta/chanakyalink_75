f99ffa1198084a6009b8cfd4bf58e6f33e691d69
Priyadharshinijutta <24A95A0424@aec.edu.in>
2026-05-12 14:46:03 +0530

    Update uart_rx.v

--- uart_rx.v
+++ uart_rx.v
@@ -1,29 +1,32 @@
-//============================================================
-// FILE : uart_rx.v
-//============================================================
+module uart_rx
+#(
+parameter CLKS_PER_BIT = 87
+)
 
-module uart_rx(
+(
     input clk,
     input rst,
+
     input rx,
 
     output reg [7:0] rx_data,
     output reg rx_done
 );
 
+reg [15:0] clk_count;
 reg [3:0] bit_index;
-reg [7:0] shift_reg;
-reg receiving;
+reg [9:0] rx_shift;
+reg busy;
 
 always @(posedge clk or posedge rst)
 begin
 
     if(rst)
     begin
+        clk_count <= 0;
         bit_index <= 0;
-        shift_reg <= 0;
+        busy <= 0;
         rx_done <= 0;
-        receiving <= 0;
     end
 
     else
@@ -31,27 +34,40 @@ begin
 
         rx_done <= 0;
 
-        if(!receiving && rx == 0)
+        if(!busy && rx == 0)
         begin
-            receiving <= 1;
+            busy <= 1;
+            clk_count <= 0;
             bit_index <= 0;
         end
 
-        else if(receiving)
+        else if(busy)
         begin
-            shift_reg[bit_index] <= rx;
-            bit_index <= bit_index + 1;
 
-            if(bit_index == 7)
+            if(clk_count < CLKS_PER_BIT-1)
             begin
-                receiving <= 0;
-                rx_data <= shift_reg;
-                rx_done <= 1;
+                clk_count <= clk_count + 1;
+            end
+
+            else
+            begin
+
+                clk_count <= 0;
+
+                rx_shift[bit_index] <= rx;
+
+                if(bit_index < 9)
+                    bit_index <= bit_index + 1;
+
+                else
+                begin
+                    busy <= 0;
+                    rx_data <= rx_shift[8:1];
+                    rx_done <= 1;
+                end
             end
         end
-
     end
-
 end
 
 endmodule
