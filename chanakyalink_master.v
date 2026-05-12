f5e1f5df80cf90156f37cbeda337bded97d6d7f8
Priyadharshinijutta <24A95A0424@aec.edu.in>
2026-05-12 14:44:57 +0530

    Update chanakyalink_master.v

--- chanakyalink_master.v
+++ chanakyalink_master.v
@@ -1,49 +1,141 @@
-//============================================================
-// FILE : chanakyalink_master.v
-//============================================================
+module chanakyalink_master #(parameter CLK_FREQ = 50000000,
+                             parameter BAUD_RATE = 9600)
 
-module chanakyalink_master(
+(
     input clk,
     input rst,
 
+    input start,
+
     input [7:0] slave_id,
+    input [7:0] command,
     input [15:0] delay_value,
 
-    output reg [7:0] tx_slave_id,
-    output reg [7:0] tx_command,
-    output reg [15:0] tx_delay,
-    output reg [7:0] tx_checksum,
-
-    output reg frame_valid
+    output tx,
+    output reg done
 );
+
+localparam CLKS_PER_BIT = CLK_FREQ / BAUD_RATE;
+
+wire tx_busy;
+reg tx_start;
+reg [7:0] tx_data;
+
+uart_tx #(CLKS_PER_BIT)
+UART_TX
+(
+    .clk(clk),
+    .rst(rst),
+    .tx_start(tx_start),
+    .tx_data(tx_data),
+    .tx(tx),
+    .tx_busy(tx_busy)
+);
+
+reg [2:0] state;
+
+localparam IDLE  = 0;
+localparam SEND1 = 1;
+localparam SEND2 = 2;
+localparam SEND3 = 3;
+localparam SEND4 = 4;
+localparam SEND5 = 5;
+localparam DONE  = 6;
+
+wire [7:0] checksum;
+
+assign checksum =
+        slave_id ^
+        command ^
+        delay_value[15:8] ^
+        delay_value[7:0];
 
 always @(posedge clk or posedge rst)
 begin
 
     if(rst)
     begin
-        frame_valid <= 0;
+        state <= IDLE;
+        tx_start <= 0;
+        tx_data <= 0;
+        done <= 0;
     end
 
     else
     begin
 
-        tx_slave_id <= slave_id;
-        tx_command <= 8'd1;
-        tx_delay <= delay_value;
+        tx_start <= 0;
 
-        tx_checksum <=
-            slave_id ^
-            8'd1 ^
-            delay_value[15:8] ^
-            delay_value[7:0];
+        case(state)
 
-        frame_valid <= 1;
+        IDLE:
+        begin
+            done <= 0;
 
-        $display("[MASTER] FRAME SENT TO SLAVE %0d", slave_id);
+            if(start)
+                state <= SEND1;
+        end
 
+        SEND1:
+        begin
+            if(!tx_busy)
+            begin
+                tx_data <= slave_id;
+                tx_start <= 1;
+                state <= SEND2;
+            end
+        end
+
+        SEND2:
+        begin
+            if(!tx_busy)
+            begin
+                tx_data <= command;
+                tx_start <= 1;
+                state <= SEND3;
+            end
+        end
+
+        SEND3:
+        begin
+            if(!tx_busy)
+            begin
+                tx_data <= delay_value[15:8];
+                tx_start <= 1;
+                state <= SEND4;
+            end
+        end
+
+        SEND4:
+        begin
+            if(!tx_busy)
+            begin
+                tx_data <= delay_value[7:0];
+                tx_start <= 1;
+                state <= SEND5;
+            end
+        end
+
+        SEND5:
+        begin
+            if(!tx_busy)
+            begin
+                tx_data <= checksum;
+                tx_start <= 1;
+                state <= DONE;
+            end
+        end
+
+        DONE:
+        begin
+            done <= 1;
+
+            if(!start)
+                state <= IDLE;
+        end
+
+        endcase
     end
-
 end
 
 endmodule
