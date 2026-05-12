9ca06ff0084bfc6f4afb2d67600a9c952169f4bd
Priyadharshinijutta <24A95A0424@aec.edu.in>
2026-05-12 11:01:15 +0530

    Update uart_rx.v

--- uart_rx.v
+++ uart_rx.v
@@ -1,109 +1,57 @@
-//==============================================================
-// FILE: uart_rx.v
-// UART RECEIVER
-//==============================================================
+//============================================================
+// FILE : uart_rx.v
+//============================================================
 
-module uart_rx #(parameter CLKS_PER_BIT = 87)
-(
+module uart_rx(
     input clk,
     input rst,
+    input rx,
 
-    input rx_serial,
-
-    output reg rx_done,
-    output reg [7:0] rx_data
+    output reg [7:0] rx_data,
+    output reg rx_done
 );
 
-    reg [3:0] state;
-    reg [15:0] clk_count;
-    reg [2:0] bit_index;
+reg [3:0] bit_index;
+reg [7:0] shift_reg;
+reg receiving;
 
-    localparam IDLE      = 0,
-               START_BIT = 1,
-               DATA_BITS = 2,
-               STOP_BIT  = 3,
-               CLEANUP   = 4;
+always @(posedge clk or posedge rst)
+begin
 
-    always @(posedge clk or posedge rst)
+    if(rst)
     begin
-        if(rst)
+        bit_index <= 0;
+        shift_reg <= 0;
+        rx_done <= 0;
+        receiving <= 0;
+    end
+
+    else
+    begin
+
+        rx_done <= 0;
+
+        if(!receiving && rx == 0)
         begin
-            state <= IDLE;
-            rx_done <= 0;
-            clk_count <= 0;
+            receiving <= 1;
             bit_index <= 0;
         end
 
-        else
+        else if(receiving)
         begin
-            case(state)
+            shift_reg[bit_index] <= rx;
+            bit_index <= bit_index + 1;
 
-            IDLE:
+            if(bit_index == 7)
             begin
-                rx_done <= 0;
-
-                if(rx_serial == 0)
-                begin
-                    clk_count <= 0;
-                    state <= START_BIT;
-                end
+                receiving <= 0;
+                rx_data <= shift_reg;
+                rx_done <= 1;
             end
-
-            START_BIT:
-            begin
-                if(clk_count == (CLKS_PER_BIT-1)/2)
-                begin
-                    if(rx_serial == 0)
-                    begin
-                        clk_count <= 0;
-                        state <= DATA_BITS;
-                    end
-                    else
-                        state <= IDLE;
-                end
-                else
-                    clk_count <= clk_count + 1;
-            end
-
-            DATA_BITS:
-            begin
-                if(clk_count < CLKS_PER_BIT-1)
-                    clk_count <= clk_count + 1;
-                else
-                begin
-                    clk_count <= 0;
-                    rx_data[bit_index] <= rx_serial;
-
-                    if(bit_index < 7)
-                        bit_index <= bit_index + 1;
-                    else
-                    begin
-                        bit_index <= 0;
-                        state <= STOP_BIT;
-                    end
-                end
-            end
-
-            STOP_BIT:
-            begin
-                if(clk_count < CLKS_PER_BIT-1)
-                    clk_count <= clk_count + 1;
-                else
-                begin
-                    rx_done <= 1'b1;
-                    clk_count <= 0;
-                    state <= CLEANUP;
-                end
-            end
-
-            CLEANUP:
-            begin
-                state <= IDLE;
-                rx_done <= 0;
-            end
-
-            endcase
         end
+
     end
+
+end
 
 endmodule
